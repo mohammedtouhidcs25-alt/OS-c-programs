1f47f0bb7b6a32bc3f252b89b5a6535e1181abb2
Mohammed Touhid Khan <mohammedtouhid.cs25@bmsce.ac.in>
2026-03-06 10:16:37 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, i;
-    int bt[20], wt[20], tat[20];
-    float avg_wt = 0, avg_tat = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    // Input burst times
-    for(i = 0; i < n; i++) {
-        printf("Enter burst time for process P%d: ", i+1);
-        scanf("%d", &bt[i]);
+    int at[n], bt[n], ct[n], tat[n], wt[n], rt[n];
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter Arrival Time and Burst Time for Process %d: ", i+1);
+        scanf("%d %d", &at[i], &bt[i]);
     }
 
-    // First process waiting time
-    wt[0] = 0;
+    // First process
+    ct[0] = at[0] + bt[0];
 
-    // Calculate waiting time
-    for(i = 1; i < n; i++) {
-        wt[i] = wt[i-1] + bt[i-1];
+    for(i = 1; i < n; i++)
+    {
+        if(ct[i-1] < at[i])
+            ct[i] = at[i] + bt[i];
+        else
+            ct[i] = ct[i-1] + bt[i];
     }
 
-    // Calculate turnaround time
-    for(i = 0; i < n; i++) {
-        tat[i] = wt[i] + bt[i];
-        avg_wt += wt[i];
-        avg_tat += tat[i];
+    for(i = 0; i < n; i++)
+    {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        rt[i] = wt[i];   // In FCFS RT = WT
     }
 
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\tRT\n");
 
-    for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    for(i = 0; i < n; i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               i+1, at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
     }
-
-    printf("\nAverage Waiting Time = %.2f", avg_wt/n);
-    printf("\nAverage Turnaround Time = %.2f\n", avg_tat/n);
 
     return 0;
 }
